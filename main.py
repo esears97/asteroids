5058d8843c3b8a6beb8699caeb9a5ebc0ce521dc
esears97 <esears97@gmail.com>
2025-05-03 21:23:14 -0400

    asteroid spawn and collision

--- main.py
+++ main.py
@@ -4,22 +4,44 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+import sys
 
 def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
+
     while True:
         screen.fill("black")
         time_passed = game_clock.tick(60)
         dt = time_passed / 1000
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for item in asteroids:
+            if item.collision_check(player) == True:
+                print("Game Over!")
+                sys.exit(0)
+
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
